6c47ee0487afdb281e690a12f1c974f8e32583e4
sayar1n <zalexhirai@gmail.com>
2025-03-10 20:45:24 +0300

    fix

--- Models/CreateNote.cs
+++
@@ -1,8 +0,0 @@
-namespace backend.Models
-{
-    public class CreateNote
-    {
-        public string Title { get; set; }
-        public string? Content { get; set; }
-    }
-}
--- Models/Product.cs
+++
@@ -1,9 +0,0 @@
-namespace backend.Models
-{
-    public class Product
-    {
-        public int Id { get; set; }
-        public required string Name { get; set; }
-        public decimal Price { get; set; }
-    }
-}
